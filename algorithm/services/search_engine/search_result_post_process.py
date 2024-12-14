89db535fc1080b0ad795695a365f486bfe6fece0
张正 <zhangzheng@zhangzhengdeMacBook-Pro.local>
2024-12-14 17:18:06 +0800

    add the search result pose process

---
+++ algorithm/services/search_engine/search_result_post_process.py
@@ -0,0 +1,82 @@
+from typing import List
+import requests
+import json
+from time import sleep
+import argparse
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))))
+from AskOnce.algorithm.services.task_manager.task_manager import TaskManager,http_post_json
+from AskOnce.algorithm.lib.search_engine.search_result_postprocess import SearchResultPostProcess
+import time
+from datetime import datetime
+import traceback
+
+class QAnswerInput:
+    def __init__(self,json_data,task_id) -> None:
+        self.search_result = json_data['search_result']
+        self.max_chunk_size = json_data['max_chunk_size'] if 'max_chunk_size' in json_data.keys() else 2048
+        self.task_id = task_id
+
+def unmarshal_task_input(GetTaskResp : dict):
+    task_type = GetTaskResp['task_type']
+    input_json = json.loads(GetTaskResp["input"])
+    if task_type ==args.tasktype[0]:
+        return  task_type,QAnswerInput(json_data=input_json, task_id=GetTaskResp["task_id"]) 
+
+def process(task_input,task_type,model,args,tm):
+    if task_type ==args.tasktype[0]:
+        start_time = time.time()
+        result_all = {'search_result':task_input.search_result} 
+        end_time = time.time()
+        print('后处理搜索结果','用时'+str(end_time-start_time))
+        return result_all
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--jobdurl", type=str, default='', help="jobd use url not use ip & port")
+    parser.add_argument("--tasktype", type=str, nargs='+', default=[], help="task_type")
+    parser.add_argument("--worker_name", type=str, default='', help="worker_name")
+    args = parser.parse_args()
+    tm = TaskManager(jobdurl=args.jobdurl)
+    model = SearchResultPostProcess() 
+    for one_task_type in args.tasktype:
+        tm.add_task_type_info(one_task_type,10000,args.worker_name)
+
+    while (True):
+        try:
+            errcode, get_task_resp = tm.get_task_block(args.tasktype)
+            if 'data' in get_task_resp.keys():
+                get_task_resp = get_task_resp['data']
+        except Exception as e:
+            if 'value' not in str(e):
+                print('gettask',e)
+            continue
+        if errcode != 200:
+            sleep(0.05)
+            print('空任务队列',datetime.now(),errcode)
+            continue
+        else:
+            print('收到任务',datetime.now())
+            try:
+                one_task_type,task_input = unmarshal_task_input(get_task_resp)
+            except Exception as e:
+                print('process input_data',e)
+                info = traceback.format_exc()
+                print(info)
+                tm.update_info(task_info={"task_id": get_task_resp["task_id"],"output": 'error',"status" : TaskManager.STATUS_INPUT_MISMATCH})
+                continue
+            try:
+                taskOutput = process(task_input,one_task_type,model,args,tm)
+            except Exception as e:
+                print('process',e)
+                info = traceback.format_exc()
+                print(info)
+                tm.update_info(task_info={"task_id": get_task_resp["task_id"],"output": 'error',"status" : TaskManager.STATUS_EXEC_FAILED})
+                continue
+            try:
+                tm.update_info(task_info={"task_id": get_task_resp["task_id"],"output": json.dumps(taskOutput,ensure_ascii=False),"status" : TaskManager.STATUS_FINISH})
+            except Exception as e:
+                traceback.print_exc()
+                print(taskOutput)
+                tm.update_info(task_info={"task_id": get_task_resp["task_id"],"output": 'error',"status" : TaskManager.STATUS_EXEC_FAILED})
+                continue
