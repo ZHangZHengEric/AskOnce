b8e198cc26966c21ff4b9747faa0ee1ce060b95f
xiangtao94 <xiangtao1994@gmail.com>
2024-12-20 22:56:14 +0800

    feat：添加supervisord

--- algorithm/build.sh
+++ algorithm/build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # 需要打包的服务文件名，标准的服务是放在相同名称文件夹下的，如：document_split/document_split.py
-services=("document_split" "answer_by_documents")
+services=(
+ document_split
+ answer_by_documents
+ data_convert
+ generate_outlines
+ result_add_reference
+ split_question
+ question_rewrite
+ net_rag_assessment
+ search_result_post_process
+ )
 mkdir -p build
 # Loop through the array and execute pyinstaller for each file
 for s in "${services[@]}"; do
-    python -m nuitka  --nofollow-imports  --enable-plugin=multiprocessing --include-package=AskOnce --output-dir=build/  --static-libpython=no -o "build/$s.bin" --remove-output services/answer_by_documents/answer_by_documents.py
+    echo "服务 $s 构建开始."
+    python -m nuitka --nofollow-imports --enable-plugin=multiprocessing --include-package=AskOnce --output-dir=build/  --static-libpython=no -o "build/$s.bin" --remove-output "services/$s/$s.py"
 # Wait for all background jobs to complete
     echo "服务 $s 构建完成."
 done
--- algorithm/services/task_manager/task_manager.py
+++ algorithm/services/task_manager/task_manager.py
@@ -30,12 +30,12 @@ class TaskManager:
     STATUS_EXEC_FAILED = "EXEC_FAILED"
     STATUS_WAITTING    = "WAITTING"
     STATUS_RUNNING     = "RUNNING"
-    def __init__(self,jobdip='127.0.0.1',jobdport=20033,jobdurl='',worker_name='') -> None:
-        self.jobdip = jobdip
-        self.jobdport = jobdport
+    def __init__(jobdurl='',worker_name='') -> None:
         self.worker_name = worker_name
         if len(jobdurl)==0:
-            self.jobdurl = 'http://{}:{}'.format(self.jobdip,self.jobdport)
+            default_jobd_url = os.getenv('JOBD_ADDR', '')
+            print('读取环境变量jobd_url:',default_jobd_url)
+            self.jobdurl = default_jobd_url
         else:
             self.jobdurl = jobdurl
         self.GetTaskUrl = "{}/jobd/worker/GetTask"
