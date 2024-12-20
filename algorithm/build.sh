cf93f2d29a616264bd6da167479602de7ab52c94
xiangtao94 <xiangtao1994@gmail.com>
2024-12-20 21:25:21 +0800

    feat：python构建可编译执行文件

--- algorithm/build.sh
+++ algorithm/build.sh
@@ -2,16 +2,11 @@
 
 # 需要打包的服务文件名，标准的服务是放在相同名称文件夹下的，如：document_split/document_split.py
 services=("document_split" "answer_by_documents")
-
+mkdir -p build
 # Loop through the array and execute pyinstaller for each file
 for s in "${services[@]}"; do
-  echo "开始构建服务 $s..."
-  (
     python -m nuitka  --nofollow-imports  --enable-plugin=multiprocessing --include-package=AskOnce --output-dir=build/  --static-libpython=no -o "build/$s.bin" --remove-output services/answer_by_documents/answer_by_documents.py
-  ) &
 # Wait for all background jobs to complete
-wait
-
-echo "所有服务构建完成."
+    echo "服务 $s 构建完成."
 done
 
