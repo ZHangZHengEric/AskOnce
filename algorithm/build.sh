5b6847ec6385eca20e28bdf91ed32a12685d8e50
xiangtao94 <xiangtao1994@gmail.com>
2024-12-20 20:53:24 +0800

    feat：python构建可编译执行文件

--- algorithm/build.sh
+++ algorithm/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # 需要打包的服务文件名，标准的服务是放在相同名称文件夹下的，如：document_split/document_split.py
-files=("document_split" "answer_by_documents")
+services=("document_split" "answer_by_documents")
 
 # Loop through the array and execute pyinstaller for each file
-for file in "${files[@]}"; do
-  echo "开始构建服务 $file..."
+for s in "${services[@]}"; do
+  echo "开始构建服务 $s..."
   (
-    pyinstaller --onefile --strip --noconfirm --distpath ./bin/ --add-data "../algorithm:AskOnce/algorithm" --name "$file" "services/$file/$file.py"
+    python -m nuitka  --nofollow-imports  --enable-plugin=multiprocessing --include-package=AskOnce --output-dir=build/  --static-libpython=no -o "build/$s.bin" --remove-output services/answer_by_documents/answer_by_documents.py
   ) &
 # Wait for all background jobs to complete
 wait
 
 echo "所有服务构建完成."
 done
+
