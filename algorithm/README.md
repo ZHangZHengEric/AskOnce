72dd0cdf89843ccf1bf47d906ea0158de5ce2438
xiangtao94 <xiangtao1994@gmail.com>
2024-12-20 23:10:19 +0800

    feat：修改asset位置

--- algorithm/README.md
+++ algorithm/README.md
@@ -1,10 +1,2 @@
 # AskOnce
 全面而简单的知识库AI搜索系统-算法服务
-
-1. 更新依赖包
-
-```shell
-
-pip3 install pipreqs
-pipreqs ./ --force
-```
--- algorithm/build.sh
+++ algorithm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export PYTHONPATH="../../:$PYTHONPATH"
 # 需要打包的服务文件名，标准的服务是放在相同名称文件夹下的，如：document_split/document_split.py
 services=(
  document_split
@@ -16,7 +16,7 @@ mkdir -p build
 # Loop through the array and execute pyinstaller for each file
 for s in "${services[@]}"; do
     echo "服务 $s 构建开始."
-    python -m nuitka --nofollow-imports --enable-plugin=multiprocessing --include-package=AskOnce --output-dir=build/  --static-libpython=no -o "build/$s.bin" --remove-output "services/$s/$s.py"
+    nuitka --nofollow-imports --enable-plugin=multiprocessing --include-package=AskOnce --output-dir=build/  --static-libpython=no -o "build/$s.bin" --remove-output "services/$s/$s.py"
 # Wait for all background jobs to complete
     echo "服务 $s 构建完成."
 done
